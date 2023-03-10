5acfe3746907daf0b5ae26b69657be1ba2c0fb25
Ezequiel Macías Rivera <ezequielmr678@gmail.com>
2023-03-09 22:04:57 -0600

    Added graph and dialog stylized

--- InterestCalculator/InterestCalculator/src/app/app.module.ts
+++ InterestCalculator/InterestCalculator/src/app/app.module.ts
@@ -1,14 +1,19 @@
-import { InfoComponent } from './info/info.component';
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { Material } from './material.module';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { NgChartsModule } from 'ng2-charts';
+import { Material } from './material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormComponent } from './calculatorForm/form/form.component';
-import { TableComponent } from './summaryTable/table/table.component';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { TableComponent } from './summaryTable/table.component';
+import { CalculatorGraphComponent } from './calculator-graph/calculator-graph.component';
+import { InfoComponent } from './info/info.component';
+import { AppComponent } from './app.component';
+
+import { FormComponent } from './calculatorForm/form.component';
+
 import { FooterComponent } from './footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [
@@ -16,17 +21,19 @@ import { HttpClientModule } from '@angular/common/http';
     InfoComponent,
     FormComponent,
     TableComponent,
-    FooterComponent
+    FooterComponent,
+    CalculatorGraphComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     Material,
-    HttpClientModule
+    HttpClientModule,
+    NgChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
   entryComponents: [InfoComponent]
 })
-export class AppModule { }
+export class AppModule {}
